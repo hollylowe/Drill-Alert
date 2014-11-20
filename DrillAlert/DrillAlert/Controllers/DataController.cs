70ed3c840368c564a156b451e0f1a7085dd273b8
Marty Loewenthal <mkavone@gmail.com>
2014-11-19 23:03:04 -0800

    added data controller

---
+++ DrillAlert/DrillAlert/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Services;
+using DrillAlert.Models;
+using Newtonsoft.Json;
+
+namespace DrillAlert.Controllers
+{
+    public class DataController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/data/{id}")]
+        public Alert Get(int id)
+        {
+            return AlertDatabse.GetAlert(id);
+        }
+    }
+}
