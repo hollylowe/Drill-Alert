a3dd32d871b2eea8bed9f00bd7563a1bd1e4cffe
Marty Loewenthal <mkavone@gmail.com>
2014-11-21 17:13:21 -0800

    Added TimePoint class and WellboreData endpoint

---
+++ DrillAlertServer/DrillAlertServer/Models/TimePoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillAlertServer.Models
+{
+    [Serializable]
+    public class TimePoint
+    {
+        internal double X;
+        internal double Y;
+        public TimePoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
