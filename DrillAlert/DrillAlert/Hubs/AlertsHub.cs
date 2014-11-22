4117588c84a8319cf31cde297f49c426eb483294
Marty Loewenthal <mkavone@gmail.com>
2014-11-21 16:36:18 -0800

    Created New Project DrillAlertServer

---
+++ DrillAlert/DrillAlert/Hubs/AlertsHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace DrillAlert.Hubs
+{
+    public class AlertsHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
