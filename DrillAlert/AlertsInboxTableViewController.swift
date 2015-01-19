ba2aa60a211fa82b8b196013eaa3de87a9f13813
Lucas David <lucashd@icloud.com>
2015-01-19 11:46:34 -0800

    Preparing for alert inbox switch

---
+++ DrillAlert/AlertsInboxTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AlertsInboxTableViewController.swift
+//  DrillAlert
+//
+//  Created by Lucas David on 1/19/15.
+//  Copyright (c) 2015 Drillionaires. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlertsInboxTableViewController: UITableViewController {
+    
+}
+
+extension AlertsInboxTableViewController: UITableViewDataSource {
+    
+}
+
+extension AlertsInboxTableViewController: UITableViewDelegate {
+    
+}
