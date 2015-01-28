3d39f4b1a3824d7566fbfbc56845071da884a993
Holly Lowe <hlowe@calpoly.edu>
2015-01-28 00:23:19 -0800

    some notes, and minor changes

--- DrillAlert/ManageVisualsTableViewController.swift
+++ DrillAlert/ManageVisualsTableViewController.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 
+//TODO populate this view with a custom table cell that is a name of a visual and switch that indicates if it will show up on the home screen or not. 
+
 class ManageVisualsNavigationController: UINavigationController {
     class func storyboardIdentifier() -> String! {
         return "ManageVisualsNavigationController"
@@ -18,48 +20,36 @@ class ManageVisualsNavigationController: UINavigationController {
 class ManageVisualsTableViewController: UITableViewController {
     // Implicit, set by the previous view controller
     var wellboreDetailViewController: WellboreDetailViewController!
-//    var parameterAlerts: [ParameterAlert]!
-//    
-//    override func viewDidLoad() {
-//        self.title = "Manage Alerts"
-//        parameterAlerts = ParameterAlert.getParameterAlertsForUser(wellboreDetailViewController.currentUser, andWellbore: wellboreDetailViewController.currentWellbore)
-//        
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "rightBarButtonItemTapped:")
-//        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: "leftBarButtonItemTapped:")
-//        
-//        super.viewDidLoad()
-//    }
-//    
-//    class func storyboardIdentifier() -> String! {
-//        return "ParameterAlertsTableViewController"
-//    }
-//    
-//    func rightBarButtonItemTapped(sender: UIBarButtonItem) {
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        
-//        let addAlertNavigationController = storyboard.instantiateViewControllerWithIdentifier(AddParameterAlertNavigationController.getStoryboardIdentifier()) as AddParameterAlertNavigationController
-//        let addParameterAlertViewController = addAlertNavigationController.viewControllers[0] as AddParameterAlertTableViewController
-//        addParameterAlertViewController.delegate = self
-//        self.presentViewController(addAlertNavigationController, animated: true, completion: nil)
-//    }
-//    
-//    func leftBarButtonItemTapped(sender: UIBarButtonItem) {
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//    }
+    
+    
+    override func viewDidLoad() {
+        self.title = "Manage Visuals"
+    
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Edit, target: self, action: "rightBarButtonItemTapped:")
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: "leftBarButtonItemTapped:")
+        
+        super.viewDidLoad()
+    }
+    
+    class func storyboardIdentifier() -> String! {
+        return "ParameterAlertsTableViewController"
+    }
+    
+    func rightBarButtonItemTapped(sender: UIBarButtonItem) {
+        //TODO: this should change the items from switches to three lines and delete icons
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "AddButtonItemTapped:")
+    }
+    
+    func leftBarButtonItemTapped(sender: UIBarButtonItem) {
+        //TODO: save all currently set states? this might be controlled elsewhere
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func AddButtonItemTapped(sender: UIBarButtonItem) {
+        print ("add a new visual")
+        //TODO: Add functionality
+    }
+    
     
 }
 
-//extension ParameterAlertsTableViewController: UITableViewDataSource {
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier(ParameterAlertTableViewCell.getCellIdentifier()) as ParameterAlertTableViewCell
-//        let parameterAlert = parameterAlerts[indexPath.row]
-//        
-//        cell.setupWithParameterAlert(parameterAlert)
-//        
-//        return cell
-//    }
-//    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return parameterAlerts.count
-//    }
-//}
